050cf9c143b8e9ce07bd687d78b42fe1fa653c47
Pushpesh Lingam <pushpeshlingam@gmail.com>
2026-04-22 12:48:12 +0530

    Changes in frontend and backend

--- backend/config/cloudinary.js
+++
@@ -1,9 +0,0 @@
-import { v2 as cloudinary } from 'cloudinary';
-
-cloudinary.config({
-  cloud_name: 'dyfqfpgec',
-  api_key: '972768314819877',
-  api_secret: process.env.CLOUDINARY_API_SECRET
-});
-
-export default cloudinary; 
--- backend/utils/response.js
+++
@@ -1,7 +0,0 @@
-export const sendResponse = (res, statusCode, data, message = '') => {
-  res.status(statusCode).json({
-    success: true,
-    data,
-    message
-  });
-}; 
